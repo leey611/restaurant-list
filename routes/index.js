237d17a9d9b88a2d9f6b42744ac9b339dd547b79
leey611 <leey611@newschool.edu>
2020-06-06 19:15:29 -0400

    add user login and register page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const home = require('./modules/home');
 const restaurants = require('./modules/restaurants');
+const users = require('./modules/users');
 
-router.use('/', home);
+router.use('/users', users);
 router.use('/restaurants', restaurants);
+router.use('/', home);
 
 module.exports = router;
