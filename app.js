5545f8a8216d956770ca3cf120eb0709b3962447
leey611 <leey611@newschool.edu>
2020-06-07 00:53:05 -0400

    implement passport js

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const exphbs = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const session = require('express-session')
+const session = require('express-session');
+const usePassport = require('./config/passport');
 const routes = require('./routes');
 require('./config/mongoose');
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+usePassport(app);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
