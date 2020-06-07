dfd8771f91f1ad032f070e8fe37a242f036edc77
leey611 <leey611@newschool.edu>
2020-06-06 21:20:10 -0400

    add express-session

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const exphbs = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const session = require('express-session')
 const routes = require('./routes');
 require('./config/mongoose');
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
